d11ba2b237bd0d071df126cce6ed91a691cb1795
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-03-08 21:17:26 -0300

    Pacotes repositories/controllers/services e endpoints - ok

---
+++ src/main/java/com/devsuperior/bds04/services/EventService.java
@@ -0,0 +1,24 @@
+package com.devsuperior.bds04.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds04.dto.EventDTO;
+import com.devsuperior.bds04.entities.Event;
+import com.devsuperior.bds04.repositories.EventRepository;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+
+	@Transactional(readOnly = true)
+	public Page<EventDTO> findAllPaged(Pageable pageable) {
+		Page<Event> list = repository.findAll(pageable);
+		return list.map(x -> new EventDTO(x));
+	}
+}
