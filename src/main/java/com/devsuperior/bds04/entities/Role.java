8634bcf688f39fe3515b627f45d5518597739027
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-03-11 19:54:18 -0300

    seed the data

--- src/main/java/com/devsuperior/bds04/entities/Role.java
+++ src/main/java/com/devsuperior/bds04/entities/Role.java
@@ -4,13 +4,18 @@ import java.io.Serializable;
 import java.util.Objects;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "tb_user")
+@Table(name = "tb_role")
 public class Role implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private String authority;
 	
